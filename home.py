79dedf4dc1b09a11f10fb2bb436065c23881eece
Varun Patel <varunpatel@Varuns-MacBook-Pro-2.local>
2023-04-30 19:38:30 -0400

    added functionality for tickets, airplane, airport, routes/leg page

--- home.py
+++ home.py
@@ -26,22 +26,22 @@ class home:
         self.button_frame.pack(pady=10)
 
         # Create the buttons with larger width and height and add them to the grid in a row-wise order
-        button1 = tk.Button(self.button_frame, text="Airplanes", width=20, height=2)
+        button1 = tk.Button(self.button_frame, text="Airplanes", width=20, height=2, command=self.airplanes)
         button1.grid(row=0, column=0, padx=10, pady=10)
 
-        button2 = tk.Button(self.button_frame, text="Routes & Legs", width=20, height=2)
+        button2 = tk.Button(self.button_frame, text="Routes & Legs", width=20, height=2, command=self.routes)
         button2.grid(row=0, column=1, padx=10, pady=10)
 
         button3 = tk.Button(self.button_frame, text="Pilots", width=20, height=2)
         button3.grid(row=1, column=0, padx=10, pady=10)
 
-        button4 = tk.Button(self.button_frame, text="Tickets", width=20, height=2)
+        button4 = tk.Button(self.button_frame, text="Tickets", width=20, height=2, command=self.tickets)
         button4.grid(row=1, column=1, padx=10, pady=10)
 
         button5 = tk.Button(self.button_frame, text="People", width=20, height=2)
         button5.grid(row=2, column=0, padx=10, pady=10)
 
-        button6 = tk.Button(self.button_frame, text="Airports", width=20, height=2)
+        button6 = tk.Button(self.button_frame, text="Airports", width=20, height=2, command=self.airports)
         button6.grid(row=2, column=1, padx=10, pady=10)
 
         button7 = tk.Button(self.button_frame, text="Flights", width=20, height=2)
@@ -53,13 +53,90 @@ class home:
         button9 = tk.Button(self.button_frame, text="Tables", width=20, height=2)
         button9.grid(row=4, column=0, padx=10, pady=10)
 
-        button10 = tk.Button(self.button_frame, text="Button 10", width=20, height=2)
-        button10.grid(row=4, column=1, padx=10, pady=10)
-
         self.mainpage.mainloop()
 
 
 
+    '''
+        airplane
+    '''
+    def airplanes(self):
+        self.airplanes = tk.Tk()
+        self.airplanes.title("Airplanes")
+        self.airplanes.geometry("800x600")
+
+        self.views_label = tk.Label(self.airplanes, text="Airplanes", font=("Arial", 20))
+        self.views_label.pack(pady=10)
+
+        # Create a frame to hold the buttons
+        self.button_frame = tk.Frame(self.airplanes)
+        self.button_frame.pack(pady=10)
+
+
+        button1 = tk.Button(self.button_frame, text="Add Airplane", width=20, height=2)
+        button1.grid(row=0, column=0, padx=10, pady=10)
+    '''
+        airports
+    '''
+    def airports(self):
+        self.airports = tk.Tk()
+        self.airports.title("Airports")
+        self.airports.geometry("800x600")
+
+        self.views_label = tk.Label(self.airports, text="Airports", font=("Arial", 20))
+        self.views_label.pack(pady=10)
+
+        # Create a frame to hold the buttons
+        self.button_frame = tk.Frame(self.airports)
+        self.button_frame.pack(pady=10)
+
+
+        button1 = tk.Button(self.button_frame, text="Add Airport", width=20, height=2)
+        button1.grid(row=0, column=0, padx=10, pady=10)
+
+    '''
+        tickets
+    '''
+    def tickets(self):
+        self.tickets = tk.Tk()
+        self.tickets.title("Tickets")
+        self.tickets.geometry("800x600")
+
+        self.views_label = tk.Label(self.tickets, text="Tickets", font=("Arial", 20))
+        self.views_label.pack(pady=10)
+
+        # Create a frame to hold the buttons
+        self.button_frame = tk.Frame(self.tickets)
+        self.button_frame.pack(pady=10)
+
+
+        button1 = tk.Button(self.button_frame, text="Add Tickets", width=20, height=2)
+        button1.grid(row=0, column=0, padx=10, pady=10)
+
+    '''
+        Routes & Legs
+    '''
+    def routes(self):
+        self.routes = tk.Tk()
+        self.routes.title("Routes & Legs")
+        self.routes.geometry("800x600")
+
+        self.views_label = tk.Label(self.routes, text="Routes & Legs", font=("Arial", 20))
+        self.views_label.pack(pady=10)
+
+        # Create a frame to hold the buttons
+        self.button_frame = tk.Frame(self.routes)
+        self.button_frame.pack(pady=10)
+
+
+        button1 = tk.Button(self.button_frame, text="Add Route", width=20, height=2)
+        button1.grid(row=0, column=0, padx=10, pady=10)
+
+        button2 = tk.Button(self.button_frame, text="Add Legs", width=20, height=2)
+        button2.grid(row=0, column=1, padx=10, pady=10)
+
+        button3 = tk.Button(self.button_frame, text="Add Routes and Legs", width=20, height=2)
+        button3.grid(row=1, column=0, padx=10, pady=10)
 
     '''
         views
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     db = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="YOUR_PASSWORD_HERE",
+            password="Teentitans!0",
             database="flight_management"
         )
     home(db)
