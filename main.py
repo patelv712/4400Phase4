0f71c188ebcc6163aef0c594a46740935f412c13
ethansu349 <132176225+ethansu349@users.noreply.github.com>
2023-05-02 01:59:44 -0400

    Add files via upload
    Basic functionality is finished for Yusen's part.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     db = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="PASSWORD",
+            password="Ahbbez@971016",
             database="flight_management"
         )
     home(db)
